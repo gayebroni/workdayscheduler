551efdcf1cad68a1f97e18018b42e8df5966feac
Gaye Proctor <gayebroni@yahoo.com>
2021-09-06 22:08:58 -0400

    add scripts to devlop

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,28 @@
 
 //WHEN I open the planner
 
-// THEN the current day is displayed at the top of the calendar
+// THEN the CURRENT DAY IS DISPLAYED AT THE TOP OF THE CALENDAR
+
+
+
+
 
 // WHEN I scroll down
 
-// THEN I am presented with time blocks for standard business hours
+// THEN I am presented with TIME BLOCKS FOR STANDARD BUSINESS HOURS
 
 // WHEN I view the time blocks for that day
 
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// THEN each time block is COLOR-CODED to indicate that whether it is in the past, present, or future
+
 
 // WHEN I click into a time block
 
 // THEN I can enter an event
 
 // WHEN I click the save button for that time block
+
+
 
 // THEN the text for that event is saved in local storage
 
